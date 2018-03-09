2bd4a6469b6f0be350b9e36cc9a50916e81aa949
Lingjie Kong <ljkong@stanford.edu>
2018-03-09 01:05:57 -0800

    update install shell to get python3 working

---
+++ code/INSTALL.sh
@@ -0,0 +1,8 @@
+sudo pip3 install -U git+https://github.com/ppwwyyxx/tensorpack.git
+sudo pip3 install opencv-python
+sudo pip3 install tornado
+sudo pip3 install --upgrade tensorflow-gpu==1.4
+sudo pip3 install cmake
+sudo pip3 install zlib1g-dev
+sudo pip3 install gym[atari]
+sudo pip3 install ffmpeg
--- code/double_a3c/INSTALL.sh
+++
@@ -1,3 +0,0 @@
-pip install -U git+https://github.com/ppwwyyxx/tensorpack.git
-pip install opencv-python
-pip install tornado
